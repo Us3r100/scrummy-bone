7f1c486c8b5b8854bb4a0a7c9d141a17e657df81
Jolo Vandenberg <Us3r100@gmail.com>
2023-10-01 22:59:49 -0500

    Added utility functions for adding and finding themes/toys

--- Assets/Scripts/ToyRepository.gd
+++ Assets/Scripts/ToyRepository.gd
@@ -4,7 +4,7 @@ extends Node
 @export var print_on_load = false
 @export var json_path = "res://Data/Toys.json"
 
-var toys:Array[ToyDefs.Toy]
+#var toys:Array[ToyDefs.Toy]
 var themes:Array[ToyDefs.ToyTheme]
 
 func load_from_json(json_path:String):
@@ -25,9 +25,51 @@ func load_from_json(json_path:String):
 				
 			var theme_obj = ToyDefs.ToyTheme.new(theme["themeName"], toys_in_theme)
 			themes.append(theme_obj)
-			toys.append_array(toys_in_theme)
+#			toys.append_array(toys_in_theme)
 	else:
 		print("JSON Parse Error ", json.get_error_message(), " in ", json_text, " at line ", json.get_error_line())
+		assert(false)
+	
+# Adds a theme to the theme array if a them with the same name does not already exist
+# returns bool: whether or not the theme was added
+func add_theme(p_theme:ToyDefs.ToyTheme) -> bool:
+	var should_add_theme = (find_theme_by_name(p_theme.theme_name) == -1)
+	if should_add_theme: 
+		themes.append(p_theme)
+	return should_add_theme
+
+# Adds a toy to the corresponding theme. Toy can only be added if theme already exists and
+# a toy of the same name does not already exist within the theme.
+# returns bool: whether or not the toy was added
+func add_toy(p_toy:ToyDefs.Toy) -> bool:
+	var theme_index = find_theme_by_name(p_toy.theme) 
+	var found_toy_index = find_toy_by_name(p_toy.toy_name)
+	var should_add_toy = theme_index != -1 and found_toy_index == Vector2(-1, -1)
+	if should_add_toy:
+		themes[theme_index].toys.append(p_toy)
+	return should_add_toy
+	
+
+# Finds the index of the theme with the given theme name
+func find_theme_by_name(p_theme_name:String) -> int:
+	var index := -1
+	for i in themes.size():
+		if themes[i].theme_name == p_theme_name:
+			index = i
+			break
+	return index
+	
+# Finds the theme index and corresponding toy index of a toy by name
+# returns Vector2: where x is the theme index and y is the toy index within the theme
+func find_toy_by_name(p_toy_name:String) -> Vector2i:
+	var ret := Vector2i(-1, -1)
+	for i in themes.size():
+		for j in themes[i].toys.size():
+			if themes[i].toys[j].toy_name == p_toy_name:
+				ret.x = i
+				ret.y = j
+				break
+	return ret
 	
 
 func print_toys():
